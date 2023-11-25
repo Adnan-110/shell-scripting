74ddf4c1e79f542dd863c8164831cf42d6937f38
Adnan <adnanshaikh11501@gmail.com>
2023-11-25 20:03:55 +0530

    Practice

--- 02-esc-sequence.sh
+++ 02-esc-sequence.sh
@@ -3,6 +3,21 @@
 echo Welcome To Shell Scripting 
 echo "This is Day 1 of our Shell Scripting"
 
+#Escape sequence characters 
+#\n : New Line
+#\t : Tab Space
+#Whenever we want to enable escape sequence character in string we need to use -e with the echo
+
+echo Before
+echo *********
 echo "Line1\nLine2"
 echo Line3
 echo Line4
+echo *********
+
+echo After
+echo *********
+echo -e "Line1 \n Line2"
+echo Line3
+echo -e "Line4\tLine5"
+echo Line6
