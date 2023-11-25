06180c66e4e39b5b580530b0943cdb898e59e4d3
Adnan <adnanshaikh11501@gmail.com>
2023-11-25 21:14:48 +0530

    Practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,8 +6,8 @@
 # The advantage & disadvantage in shell is that there are no Data types.
 # In bash everything will be considered as string by default.
 
-a = 10
-b = Ten
+a=10
+b=Ten
 
 # How can we print a variable in scripting ?
 # To print a variable u need to start with a $ sign.
