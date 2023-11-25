007914e6a50597ca5e7e3764d2b019acdefc285d
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 00:31:16 +0530

    Practice

--- 05-dynamic-variables.sh
+++ 05-dynamic-variables.sh
@@ -7,8 +7,8 @@
 
 DATE=$(date +%x)    #Here $() is syntax and inside them we have written date cmd of linux with option %x and + we use when we want to use option which starts with % or maybe anyother which I don't know yet
 
-echo "Good Morning, Today's date is $DATE"
+echo -e "Good Morning, Today's date is \e[32m $DATE \e[0m"
 
 NO_OF_SESSION=$(who|wc -l)      #Here who cmd show the active sessions and when we use that wc -l it will return the count of line 
 
-echo "Total Number of Connected Sessions : $NO_OF_SESSION"
+echo -e "Total Number of Connected Sessions : \e[32m $NO_OF_SESSION \e[0m"
