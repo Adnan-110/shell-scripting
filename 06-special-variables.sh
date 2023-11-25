deabdadf34a6de935ddb046befa5ae9b4ba769c1
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 00:59:55 +0530

    Practice

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -14,11 +14,10 @@ echo "Third Argument is : $3 "
 # you can provide more than 9 arguments, but suppose u have provided 10 arguments then 1st argument will be nullified from $1 
 # and 10th argument will become $1, Likewise it will go for further 
 
-echo $0   # Prints Script Name 
-echo $#   # Prints the overall arguments used in the script 
-echo $?   # This prints the exit code of the last command
-echo $*   # Prints all the arguments used.
-echo $@   # Prints all the arguments used.
+echo "Total Number of Arguments are : $# "   # Prints the overall arguments used in the script 
+echo "Exit Code of the last command is : $? "   # This prints the exit code of the last command
+echo "Print all the arguments are used : $* "   # Prints all the arguments used.
+echo "Print all the arguments are used : $@ "  # Prints all the arguments used.
 
 # echo "Variables Used In The Script $*"    # $* is going to print the used variables  
 # echo "Variabels used are $@"              # $@ is going to print the used variables  
