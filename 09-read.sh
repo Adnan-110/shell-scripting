2a4b19cc05f961b92e16393b9bf44a0bc8f88bb6
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 01:51:26 +0530

    Practice

--- 09-read.sh
+++ 09-read.sh
@@ -3,4 +3,5 @@
 read -p "Enter your name:" name   #its used take input from user
 echo "Name of the person is $name"
 
-# Here echo will print whatever written and then 
+# Here read statement which is enclosed with " " will be print and ask for user input and user input will be stored in name variable
+#In next line we use echo cmd to print the name variable
