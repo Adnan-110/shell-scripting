30d02971ee65c5cb9256609c3e66361bb5629d8d
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 00:22:17 +0530

    Practice

--- 05-dynamic-variables.sh
+++ 05-dynamic-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-DATE="17JULY2023"
+ 
+# Dynamic Variable are something which will have linux command stored on them.
+# Example below
+
+
+DATE=$(date +%x)    #Here $() is syntax and inside them we have written date cmd of linux with option %x and + we use when we want to use option which starts with % or maybe anyother which I don't know yet
 
 echo "Good Morning, Today's date is $DATE"
