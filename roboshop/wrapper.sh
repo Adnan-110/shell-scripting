37241eaf5542279c4ba656d1193073fc3747bb0c
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 18:01:19 +0530

    Configuring RoboShop Project Components

--- roboshop/wrapper.sh
+++ roboshop/wrapper.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-bash components/frontend.sh
+#bash components/frontend.sh
+
+bash components/$1.sh
+
+if [ $? -ne 0 ] ; then
+    echo -e "\e[36m Example Usage : \e[0m bash wrapper.sh componentName"
+    exit 30
+fi
+
 
