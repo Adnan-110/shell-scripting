f8e2cfeb141865458ae7278e689d97f515fa9f2c
Adnan <adnanshaikh11501@gmail.com>
2023-11-26 20:48:35 +0530

    Configuring RoboShop Project Components

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
+COMPONENT=frontend
+LOG_FILE="/tmp/${COMPONENT}.log"
+
+stat(){
+    if  [ $1 -eq 0 ] ; then      #Since $? is first arg while calling function so we are checking using $1
+    echo -e "\e[32m Success \e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
+}
 
 if [ $USER_ID -ne 0 ] ; then
     echo -e "\e[31m !!!!!!!!!!!! This Script is Expected to be Executed with sudo or as a root user !!!!!!!!!!!!\e[0m"
@@ -12,32 +22,44 @@ echo -e "************\e[33m Configuring the FrontEnd Component \e[0m************
 
 echo -n "Installing Nginx :" 
 
-yum install nginx -y    &>> /tmp/frontend.log
-
-if  [ $? -eq 0 ] ; then
-    echo -e "\e[32m Successfully Completed \e[0m"
-else
-    echo -e "\e[31m Failed to Install \e[0m"
-fi
+yum install nginx -y    &>> $LOG_FILE
+stat $?
+# if  [ $? -eq 0 ] ; then
+#     echo -e "\e[32m Successfully Completed \e[0m"
+# else
+#     echo -e "\e[31m Failed to Install \e[0m"
+# fi
     
-echo -n "Downloading the Component $1 :"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
-if  [ $? -eq 0 ] ; then
-    echo -e "\e[32m Successfully Completed \e[0m"
-else
-    echo -e "\e[31m Failed to Download \e[0m"
-fi
+echo -n "Downloading the ${COMPONENT} Component :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $?
+# if  [ $? -eq 0 ] ; then
+#     echo -e "\e[32m Successfully Completed \e[0m"
+# else
+#     echo -e "\e[31m Failed to Download \e[0m"
+# fi
 
+echo -n "Clean-Up of ${COMPONENT} Component :"
+cd /usr/share/nginx/html
+rm -rf *    &>> $LOG_FILE
+stat $?
 
-# systemctl enable nginx
-# systemctl start nginx
+echo -n "Extracting of ${COMPONENT} Component :"
+unzip /tmp/frontend.zip     &>> $LOG_FILE
+stat $?
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#systemctl daemon reload
-#systemctl restart nginx
+echo -n "Configuration of ${COMPONENT} Component :"
+mv ${COMPONENT}-main/* .
+mv static/* .
+rm -rf ${COMPONENT}-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Restarting the ${COMPONENT} Component :"
+systemctl enable nginx      &>> $LOG_FILE
+systemctl daemon reload     &>> $LOG_FILE
+systemctl restart nginx     &>> $LOG_FILE
+stat $?
+
+echo -e "************\e[33m Configuration of the FrontEnd Component is Completed \e[0m************"
+#echo -e "***** \e[35m $COMPONENT Configuration Is Complted \e[0m ******"
