8f95e767f502a6bda3e33b907636006b68cea3f2
Adnan <adnanshaikh11501@gmail.com>
2023-11-27 18:07:15 +0530

    Configuring RoboShop Project Component DRY Code

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -5,6 +5,7 @@ COMPONENT=catalogue
 echo -e "************\e[33m Configuring the Cart Component \e[0m************"
 
 source components/common.sh
+NodeJS  #Calling a NodesJS function from common.sh 
 
 echo -e "************\e[33m Configuration of the Cart Component is Completed \e[0m************"
 
--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -5,6 +5,7 @@ COMPONENT=user
 echo -e "************\e[33m Configuring the User Component \e[0m************"
 
 source components/common.sh
+NodeJS  #Calling a NodesJS function from common.sh 
 
 echo -e "************\e[33m Configuration of the User Component is Completed \e[0m************"
 
