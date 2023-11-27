673401eb07906bcae2188b5159e96f7bffd7c091
Adnan <adnanshaikh11501@gmail.com>
2023-11-28 01:13:14 +0530

    Configuring RoboShop Project Component DRY Code

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,22 @@
 #!/bin/bash 
 
+COMPONENT=mysql
+COMPONENT_URL="https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo"
+source components/common.sh
+
 echo -e "************\e[33m Configuring the MySQL \e[0m************"
+
+echo -n "Configuring the ${COMPONENT} Repos :"
+curl -s -L -o /etc/yum.repos.d/${COMPONENT}.repostat $COMPONENT_URL
+stat $?
+
+echo -n "Installing the ${COMPONENT} :"
+yum install ${COMPONENT}-community-server -y   &>> $LOG_FILE
+stat $?
+
+echo -n "Starting the ${COMPONENT} :"
+systemctl enable mysqld     &>> $LOG_FILE
+systemctl start mysqld      &>> $LOG_FILE
+stat $?
+
+echo -e "************\e[33m Configuration of the MySQL is Completed \e[0m************"
