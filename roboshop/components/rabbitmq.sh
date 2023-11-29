ab4970e1ef553ec5c84270c437c7b096bc501a9e
Adnan <adnanshaikh11501@gmail.com>
2023-11-29 18:56:42 +0530

    Configuring RoboShop Project Shipping  Component

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
+COMPONENT=rabbitmq
+source components/common.sh
+
 echo -e "************\e[33m Configuring the RabbitMQ \e[0m************"
 
+echo -n "Configuring the $COMPONENT Repos :"
+curl -s https://packagecloud.io/install/repositories/${COMPONENT}/erlang/script.rpm.sh | sudo bash      &>>LOG_FILE
+curl -s https://packagecloud.io/install/repositories/${COMPONENT}/${COMPONENT}-server/script.rpm.sh | sudo bash    &>> $LOG_FILE
+stat $?
+
+echo -n "Installing the $COMPONENT :"
+yum install rabbitmq-server -y  &>> $LOG_FILE
+stat $?
+
+START_SVS
+
+rabbitmqctl list_users | grep ${APPUSER}  &>> $LOG_FILE
+if [ $? -ne 0 ] ; then
+    echo -n "Creating $APPUSER for $COMPONENT : "
+    rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
+    stat $?
+fi
+
+echo -n "Providing Permission to $APPUSER : "
+rabbitmqctl set_user_tags roboshop administrator    &>> $LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"        &>> $LOG_FILE
+stat $?
+
+echo -e "************\e[33m Configuration of the RabbitMQ is Completed \e[0m************"
